624e9537dc3370581cdf5ac7b2ccfac67e9c365f
regentje <vdregen@yandex.ru>
2020-10-22 21:35:58 +0300

    4 урок часть первая

---
+++ lesson4/protoTask2.js
@@ -0,0 +1,28 @@
+﻿let Box = function (a, b) {
+  this.sideA = a;
+  this.sideB = b;
+}
+let Rectangle = function (a, b) {
+  this.sideA = a;
+  this.sideB = b;
+}
+
+Box.prototype = {
+  scuare: function () {
+    console.log('scuare' + ':' + this.sideA * this.sideB);
+  },
+  perimeter: function () {
+    console.log('perimeter' + ':' + 2 * (this.sideA * this.sideB));
+  }
+
+}
+Rectangle.prototype = new Box();
+
+let testBox = new Box(3, 4);
+let testRectangle = new Rectangle(55, 4);
+
+
+testBox.perimeter();
+testBox.scuare();
+testRectangle.scuare();
+testRectangle.perimeter();
---
+++ lesson4/protoTask3.js
@@ -0,0 +1,35 @@
+﻿const human = {
+  name: "John",
+  lastName: "Smith",
+  phoneNumber: "+1234567890"
+}
+const employees = {
+  position: "Senior engineer",
+  startDate: "10.10.1990",
+  endDate: "10.10.2000",
+  baseSalary: "10000",
+  salaryCurrency: "$",
+  location: "Russia",
+  department: "IT",
+}
+Object.setPrototypeOf(human, employees);
+
+const ability = {
+  eat: function () { },
+  sleep: function () { },
+  callFriend: function () { },
+  writeReport: function () { },
+  organizeMeeting: function () { },
+  retire: function () { },
+  startVacation: function () { },
+  currentEmployee: function () {
+    if (this.endDate === undefined) {
+      console.log('Нынешний сотрудник');
+    } else {
+      console.log('бывший сотрудник')
+    }
+  }
+};
+
+Object.setPrototypeOf(employees, ability);
+human.currentEmployee();
