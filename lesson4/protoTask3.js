2bb86a82cc5df1570f783d056cb884e053652dbf
regentje <vdregen@yandex.ru>
2020-11-02 22:04:05 +0300

    решение задания по прототипам

--- lesson4/protoTask3.js
+++ lesson4/protoTask3.js
@@ -1,43 +1,68 @@
-﻿const СurrentEmployee = function () {
+﻿const Human = function (newHuman) {
+  this.name = newHuman.name;
+  this.lastName = newHuman.lastName;
+  this.phoneNumber = newHuman.phoneNumber;
+  this.eat = function () { };
+  this.sleep = function () { console.log('sleep') };
+  this.callFriend = function () { };
+}
+
+const Employees = function (employee) {
+  Human.call(this, employee)
+  this.position = employee.position;
+  this.startDate = employee.startDate;
+  this.baseSalary = employee.baseSalary;
+  this.salaryCurrency = employee.salaryCurrency;
+  this.location = employee.location;
+  this.department = employee.department;
+  this.writeReport = function () { console.log('writeReport') };
+  this.organizeMeeting = function () { console.log('organizeMeeting') };
+}
+const СurrentEmployee = function (humanObj) {
+  Employees.call(this, humanObj)
   this.retire = function () {
     console.log('заявление на увольнение')
   }
 }
-const FormerEmployee = function () {
+const FormerEmployee = function (former) {
+  Employees.call(this, former)
+  this.endDate = former.endDate;
   this.startVacation = function () {
     console.log('вернуться на старую работу')
   }
 }
-const Employees = function (position, startDate, baseSalary, salaryCurrency, location, department,endDate) {
-  if(endDate == undefined ) {
-    СurrentEmployee.call(this);
-  }else{
-    FormerEmployee.call(this);
-  }
-  this.position = position;
-  this.startDate = startDate;
-  this.endDate = endDate;
-  this.baseSalary = baseSalary;
-  this.salaryCurrency = salaryCurrency;
-  this.location = location;
-  this.department = department;
-  this.writeReport = function () { console.log('writeReport') };
-  this.organizeMeeting = function () { console.log('organizeMeeting') };
-  
-}
 
-const Human = function (name, lastName, phoneNumber,position, startDate, baseSalary, salaryCurrency, location, department,endDate ) {
-  Employees.call(this, position, startDate, baseSalary, salaryCurrency, location, department,endDate)
-  this.name = name;
-  this.lastName = lastName;
-  this.phoneNumber = phoneNumber;
-  this.eat = function () { };
-  this.sleep = function () {console.log('sleep') };
-  this.callFriend = function () { };
-}
+Employees.prototype = Object.create(Human.prototype);
+СurrentEmployee.prototype = Object.create(Employees.prototype);
+FormerEmployee.prototype = Object.create(Employees.prototype);
 
-const alex = new Human("name", "lastName", "phoneNumber", "position", "startDate", "baseSalary", "salaryCurrency", "location", "department","25.05.2020");
+const john = {
+  name: "John",
+  lastName: "Smith",
+  position: "Senior engineer",
+  startDate: "10.10.1990",
+  baseSalary: "10000",
+  salaryCurrency: "$",
+  location: "Russia",
+  department: "IT",
+  phoneNumber: "+1234567890",
+};
+const alex = {
+  name: "Alex",
+  lastName: "Ivanov",
+  position: "engineer",
+  startDate: "10.10.1990",
+  endDate: "10.10.2000",
+  baseSalary: "10000",
+  salaryCurrency: "$",
+  location: "Russia",
+  department: "IT",
+  phoneNumber: "+1234567890",
+};
 
-console.log(alex);
-alex.writeReport();
-alex.startVacation();
+const newJohn = new СurrentEmployee(john);
+console.log(newJohn)
+newJohn.retire();
+const newAlex = new FormerEmployee(alex);
+console.log(newAlex)
+newAlex.startVacation();
